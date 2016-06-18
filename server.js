039b9556d12e347844f44f9cd71342cbadd145e3
Surya Prakash Kaipa <suryaprakash.kaipa@kore.com>
2016-06-18 13:20:22 +0530

    Env files committed.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
 
-app.get('/mycontact', function(req, res){
+app.get('/', function(req, res){
 	res.send("Hello");
 });
 
 
-app.listen(3000);
-console.log("Server is running on port 3000");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
